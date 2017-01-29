ba7f2feb5c84fe4145b3f19c3d38d2377fc65752
Lars Jarlvik <lars.jarlvik@gmail.com>
2017-01-29 16:58:34 +0700

    Improved bitmap loading

--- src/Collada/Model/Material.cs
+++ src/Collada/Model/Material.cs
@@ -1,21 +1,24 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Text;
 using ColladaParser.Common;
-using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
 namespace ColladaParser.Collada.Model
 {
-	public class Material 
+    public class Material 
 	{
 		private const int BITMAP_HEADER_LENGTH = 54;
-		private const int BITMAP_SIZE = 1024;
 
 		private const int GL_LINEAR = 0x2601;
 		private const int GL_LINEAR_MIPMAP_LINEAR = 0x2703;
 		private const float GL_CLAMP_TO_EDGE = 0x812F;
 
 		private bool haveTexture = false;
+
+		private int textureWidth;
+		private int textureHeight;
 
 
 		private string fileName;
@@ -25,6 +28,22 @@ namespace ColladaParser.Collada.Model
 		{
 			this.fileName = fileName;
 		}
+
+		private int parseHeader(byte[] header) 
+		{
+			var fileType = Encoding.ASCII.GetString(header.Take(2).ToArray());
+			if (fileType != "BM")
+				throw new ApplicationException($"Texture has invalid file type, expected BM got {fileType}!");
+
+			var compression = BitConverter.ToInt32(header, 30);
+			if (compression != 0)
+				throw new ApplicationException("Compressed bitmaps are not supported!");
+
+			textureWidth = BitConverter.ToInt32(header, 18);
+			textureHeight = BitConverter.ToInt32(header, 22);
+
+			return BitConverter.ToInt32(header, 10); // Start of image data
+		}
 			
 		public unsafe void LoadTexture(string texturePath)
 		{
@@ -32,10 +51,18 @@ namespace ColladaParser.Collada.Model
 				return;
 
 			var imageStream = SourceLoader.AsStream($"{texturePath}.{fileName}");
-			var buffer = new byte[BITMAP_SIZE * BITMAP_SIZE * 3];
+			if (imageStream == null)
+				throw new ApplicationException($"Texture resource '{texturePath}.{fileName}' not found!");
+			
+			// Read bitmap header
+			var header = new byte[BITMAP_HEADER_LENGTH];
+			imageStream.Read(header, 0, BITMAP_HEADER_LENGTH);
+			var start = parseHeader(header);
 
-			imageStream.Seek(BITMAP_HEADER_LENGTH, SeekOrigin.Begin);
-			imageStream.Read(buffer, 0, BITMAP_SIZE * BITMAP_SIZE * 3);
+			// Read bitmap data
+			var buffer = new byte[textureWidth * textureHeight * 3];
+			imageStream.Seek(start, SeekOrigin.Begin);
+			imageStream.Read(buffer, 0, textureWidth * textureHeight * 3);
 
 			fixed (byte* p = buffer)
 			{
@@ -45,7 +72,7 @@ namespace ColladaParser.Collada.Model
 				GL.BindTexture(TextureTarget.Texture2D, textureId);
 				GL.PixelStore(PixelStoreParameter.UnpackAlignment, 1);
 				
-				GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, BITMAP_SIZE, BITMAP_SIZE, 0, PixelFormat.Bgr, PixelType.UnsignedByte, ptr);
+				GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, textureWidth, textureHeight, 0, PixelFormat.Bgr, PixelType.UnsignedByte, ptr);
 
 				GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
 				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, new [] { GL_LINEAR });
