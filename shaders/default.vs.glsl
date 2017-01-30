61394f72978ea560930377fa1f6d98ac6ad42715
Lars Jarlvik <lars.jarlvik@gmail.com>
2017-01-30 22:09:12 +0700

    Use static light position

--- shaders/default.vs.glsl
+++ shaders/default.vs.glsl
@@ -2,9 +2,9 @@
 
 precision highp float;
 
-const vec3 lightDirection = normalize(vec3(0.2, -0.3, -0.8));
+const vec3 lightDirection = vec3(-1.0, -0.8, -1.0);
 const vec3 ambientColor = vec3(0.4, 0.4, 0.4);
-const vec3 directionColor = vec3(0.7, 0.7, 0.7);
+const vec3 diffuseColor = vec3(0.7, 0.7, 0.7);
 
 uniform mat4 projection_matrix;
 uniform mat4 modelview_matrix;
@@ -23,9 +23,9 @@ void main(void)
 {
 	vec4 position = projection_matrix * modelview_matrix * vec4(in_position, 1.0);
 	
-	vec3 lightDirection = normalize(lightDirection - position.xyz);
+	vec3 lightDirection = (vec4(-lightDirection, 1.0) * modelview_matrix).xyz;
 	float directionalLightWeighting = max(dot(in_normal, lightDirection), 0.0);
-	vLightWeighting = ambientColor + directionColor * directionalLightWeighting;
+	vLightWeighting = ambientColor + diffuseColor * directionalLightWeighting;
 
 	color = in_color;
 	uv = in_texture;
